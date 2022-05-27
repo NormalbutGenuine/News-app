f3a61ea40e760293f8b42431ea16379e0d589b5e
NormalbutGenuine <obybkim@hanmail.com>
2022-05-27 21:32:39 +0900

    바이오 뉴스 크롤링 작업 중

--- news_board_crawler/sheets/Data_CreateAndUpdate.ts
+++ news_board_crawler/sheets/Data_CreateAndUpdate.ts
@@ -10,12 +10,12 @@ async function GetSheetsInstance() {
 
 export async function UpdateDataToSheets(memberArray : Array<string[]>) {
     const sheets = await GetSheetsInstance()
-    const res = await sheets.spreadsheets.values.update(configOBJ(memberArray)); // update는 데이터가 있는 셀만 적용된다.
+    const res = await sheets.spreadsheets.values.update(configOBJ(memberArray)) // update는 데이터가 있는 셀만 적용된다.
     return res
 }
 
 export async function InsertDataToSheets(memberArray : Array<string[]>) {
     const sheets = await GetSheetsInstance()
-    const res = await sheets.spreadsheets.values.append(configOBJ(memberArray));
+    const res = await sheets.spreadsheets.values.append(configOBJ(memberArray))
     return res
 }
